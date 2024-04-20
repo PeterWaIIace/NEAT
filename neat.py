e418f04e7ee207f224f64957846e0486a42295c1
Wallace <piotr.walas.prywatny@gmail.com>
2024-04-20 12:23:06 +0200

    adding growing capabilities

--- neat.py
+++ neat.py
@@ -2,6 +2,7 @@
 
 import jax.numpy as jnp 
 import jax.random as jrnd
+import random 
 from enum import Enum
 
 class NodeTypes(Enum):
@@ -21,6 +22,11 @@ class StatefulRandomGenerator:
         indecies = jrnd.permutation(self.key, val_range)[:rnd_value_int]
         self.key = jrnd.split(self.key,1)[0]
         return indecies
+    
+    def randint(self,max=100,min=0):
+        rnd_value = jrnd.randint(self.key, shape=(1,), minval=min, maxval=max)
+        self.key = jrnd.split(self.key,1)[0]
+        return rnd_value
 
 Rnd = StatefulRandomGenerator()
 
@@ -30,22 +36,60 @@ class Genome:
         self.connections_length = 20
         self.nodes_length = 20
         self.max_innov = 0
+        self.index = 0
+        self.fitness = 1.0
         # helper indicies names
+        self.i = 2
+        self.o = 3
         self.w = 4
+        self.enabled = 5
 
         # Connections genomes is array, rows are genomes, but cols are parameters of that genomes
         self.con_gen  = jnp.zeros((self.connections_length,6),)
         self.node_gen = jnp.zeros((self.nodes_length,2),)
 
+    def new_fitness(self,fit : float):
+        ''' Assign new fitness to this genome '''
+        self.fitness = fit
+
     def add_node(self,index,type):
         ''' Adding node '''
-
         if self.nodes_length <= index:
             self.nodes_length += 20
             new_nodes_spaces = jnp.zeros((20,6),)
             self.node_gen = jnp.concatenate((self.node_gen,new_nodes_spaces), axis=1)
 
-        self.node_gen.at[index].set(jnp.array([index,type.value]))
+        self.node_gen = self.node_gen.at[index].set(jnp.array([index,type.value]))
+
+    def add_r_connections(self,innov):
+        possible_input_nodes  = self.node_gen[self.node_gen[:,0] != 0 and self.node_gen[:,1] != NodeTypes.OUTPUT][:,0]
+        possible_output_nodes = self.node_gen[self.node_gen[:,0] != 0 and self.node_gen[:,1] != NodeTypes.INPUT][:,0]
+
+        in_node = possible_input_nodes[Rnd.randint(max=len(possible_input_nodes))]
+        out_node = possible_output_nodes[Rnd.randint(max=len(possible_output_nodes))]
+        self.add_connection(innov,in_node,out_node,1.0)
+
+    def add_r_node(self,innov):
+        self.index += 1
+        exisitng_connections = self.con_gen[self.con_gen[:,0] != 0]
+
+        existing_connection = exisitng_connections[Rnd.randint(max=len(exisitng_connections))]
+        index_of_connection = existing_connection[0] - 1
+        self.con_gen.at[index_of_connection].set(self.con_gen.at[index_of_connection,self.enabled].set(0.0))
+
+        new_node = self.index
+
+        self.add_connection(innov,
+                            self.con_gen.at[index_of_connection,self.i],
+                            new_node,
+                            self.con_gen.at[index_of_connection,self.w]
+                        )
+        self.add_connection(innov,
+                            new_node,
+                            self.con_gen.at[index_of_connection,self.o],
+                            self.con_gen.at[index_of_connection,self.w]
+                        )
+
 
     def add_connection(self,innov,in_node,out_node,weight):
         ''' Adding connection '''
@@ -58,31 +102,40 @@ class Genome:
             self.nodes_length += 20
             new_connections_spaces = jnp.zeros((20,6),)
             self.con_gen = jnp.concatenate((self.con_gen,new_connections_spaces), axis=1)
+        
+        if self.node_gen[in_node][0] == 0:
+            self.add_node(in_node,NodeTypes.NODE)
 
-        self.con_gen.at[innov].set(jnp.array([innov,innov,in_node,out_node,weight,1.0]))
+        if self.node_gen[out_node][0] == 0:
+            self.add_node(out_node,NodeTypes.NODE)
 
-def δ( genome_1, genome_2, c1=1.0, c2=1.0, c3=1.0, N=1.0):
+        self.con_gen = self.con_gen.at[innov].set(jnp.array([innov,innov,in_node,out_node,weight,1.0]))
+
+# assuming that population is class having con_gens of size N_POPULATION x CREATURE_GENES x All information
+# For example 50 x 10 x 6 
+# this could be done much faster
+def δ(genome_1, genome_2, c1=1.0, c2=1.0, c3=1.0, N=1.0):
     ''' calculate compatibility between genomes '''
     D = 0  # disjoint genes
     E = 0  # excess genes
 
     # check smaller innovation number:
-    innovation_thresh = genome_1 if genome_1.max_innov < genome_2.max_innov else genome_2
+    innovation_thresh = genome_1.max_innov if genome_1.max_innov < genome_2.max_innov else genome_2.max_innov
 
     D_tmp = jnp.subtract(
-        (genome_1.con_gen[:innovation_thresh,0]
-        ,genome_2.con_gen[:innovation_thresh,0])
+        genome_1.con_gen[:innovation_thresh,0]
+        ,genome_2.con_gen[:innovation_thresh,0]
     ) # getting all disjoint connections - if array do not have connection then it will have 0 as innovtaion number
-    D = len(D_tmp[D_tmp[:,0] != 0])
+    D = len(D_tmp[D_tmp != 0])
 
     E_tmp = jnp.subtract(
         genome_1.con_gen[innovation_thresh:,0]
         ,genome_2.con_gen[innovation_thresh:,0]
     ) # getting all disjoint connections - if array do not have connection then it will have 0 as innovtaion number
-    E = len(E_tmp[E_tmp[:,0] != 0])
+    E = len(E_tmp[E_tmp != 0])
 
-    W_1 = genome_1.con_gen[D_tmp[:,0] == 0][genome_1.w]
-    W_2 = genome_2.con_gen[D_tmp[:,0] == 0][genome_2.w]
+    W_1 = jnp.sum(genome_1.con_gen[:innovation_thresh,:][D_tmp == 0][genome_1.w])
+    W_2 = jnp.sum(genome_2.con_gen[:innovation_thresh,:][D_tmp == 0][genome_2.w])
     W_avg = jnp.subtract(W_1,W_2)/W_1.size
 
     d = (c1 * E) / N + (c2 * D) / N + c3 * W_avg
@@ -92,21 +145,79 @@ def sh(δ,δ_t = 0.2):
     ''' sharing fitness threshold function '''
     return δ < δ_t
 
+# population can be matrix of shapex N_SIZE_POP x 
+def speciate(population) -> list:
+    """function for speciation"""
+    δ_th = 10
+    species = [[population[0]]]
+
+    individual_1 = population[0]
+    for _,individual_2 in enumerate(population):
+        if individual_1 is not individual_2:
+            if sh(δ(individual_1,individual_2),δ_th):
+                species[len(species) - 1].append(individual_2)
+
+    for _,individual_1 in enumerate(population):
+
+        # if not in current species, create new specie
+        if sum([individual_1 in specie for specie in species]) == 0:
+            species.append([individual_1])
+            for _,individual_2 in enumerate(population):
+                if individual_1 is not individual_2:
+                    if sh(δ(individual_1,individual_2),δ_th):
+                        species[len(species) - 1].append(individual_2)
+
+    return species
 
 def mate(superior : Genome, inferior : Genome):
     ''' mate superior Genome with inferior Genome '''
     # check smaller innovation number:
     innovation_thresh = superior.max_innov if superior.max_innov < inferior.max_innov else inferior.max_innov
 
+    offspring = jnp.copy(inferior)
+
     indecies = Rnd.randint_permutations(innovation_thresh)
-    inferior.con_gen.at[indecies].set(superior.con_gen[indecies])
+    offspring.con_gen = offspring.con_gen.at[indecies].set(superior.con_gen[indecies])
 
     indecies = Rnd.randint_permutations(len(inferior.con_gen[innovation_thresh:])) + innovation_thresh
-    inferior.con_gen.at[indecies].set(superior.con_gen[indecies])
+    offspring.con_gen = offspring.con_gen.at[indecies].set(superior.con_gen[indecies])
     # Lazy but working, copy all nodes not existing in inferior but exisitng in superior
-    inferior.node_gen.at[inferior.node_gen[:,0] == 0].set(superior.node_gen[inferior.node_gen[:,0] == 0])
+    offspring.node_gen = offspring.node_gen.at[inferior.node_gen[:,0] == 0].set(superior.node_gen[inferior.node_gen[:,0] == 0])
 
-    return inferior
+    return offspring
+
+# this can be done better on arrays
+def cross_over(population,keep_top = 2):
+    ''' cross over your population '''
+    new_population = []
+    species = speciate(population)
+
+    for specie in species:
+        sorted_specie = sorted(specie, key=lambda x: x.fitness, reverse=True)[:]
+
+        org_length = len(sorted_specie)
+        sorted_specie = sorted_specie[:keep_top]
+
+        for keept in sorted_specie:
+            new_population.append(keept)
+
+        for n in range(org_length-len(new_population)):
+            n = random.randint(0,len(sorted_specie))
+            m = n
+            while m == n:
+                m = random.randint(0,len(sorted_specie))
+
+            offspring = mate(sorted_specie[n],sorted_specie[m])
+            new_population.append(offspring)
+
+def random_mutate(population):
+    for n,_ in enumerate(population):
+
+        mutation_chance = random.randint(0,10)
+        if mutation_chance  > 7:
+            population[n].add
+
+    pass
 
 if __name__=="__main__":
     superior = Genome()
@@ -122,7 +233,6 @@ if __name__=="__main__":
     inferior.add_node(0,NodeTypes.INPUT)
     inferior.add_node(1,NodeTypes.INPUT)
     inferior.add_node(2,NodeTypes.NODE)
-    inferior.add_node(3,NodeTypes.NODE)
     inferior.add_node(4,NodeTypes.NODE)
     inferior.add_node(5,NodeTypes.OUTPUT)
 
@@ -139,4 +249,11 @@ if __name__=="__main__":
     inferior.add_connection(4,2,5,1.0)
     inferior.add_connection(5,4,5,1.0)
 
-    inferior = mate(superior,inferior)
+    population = [superior, inferior]
+    print(speciate(population))
+
+    # print(inferior.con_gen)
+    # print(inferior.node_gen)
+    # inferior = mate(superior,inferior)
+    # print(inferior.con_gen)
+    # print(inferior.node_gen)
--- test.jax.py
+++ test.jax.py
@@ -1,27 +1,37 @@
-import time
+import numpy as np
 import jax.numpy as jnp
-import jax.random as jrnd
+import time
 
-superior = jnp.array([[1,4], [3,6], [6,2], [3,9], [6,2], [3,9]])
-inferior = jnp.array([[2,4], [0,0], [6,7], [2,6], [0,0], [2,6]])
+iterations = 20000
+array1 = [1, 2, 3, 4, 5]
 
-rnd_key = jrnd.PRNGKey(100)
-rnd_value = jrnd.uniform(rnd_key)
-rnd_value_int = int(rnd_value  * len(superior))
-indecies = jrnd.permutation(rnd_key, superior.shape[0])[:rnd_value_int]
+for n in range(iterations):
+    start = time.time()
+    result = [1, 2, 3, 4, 5]
+    for i in range(len(result)):
+        result[i] += 10
+    end = time.time()
 
-rnd_key = jrnd.split(rnd_key,1)[0]
-rnd_value = jrnd.uniform(rnd_key)
-rnd_value_int = int(rnd_value  * len(superior))
-indecies = jrnd.permutation(rnd_key, superior.shape[0])[:rnd_value_int]
+loop_time = end - start
+print("For loop time:", loop_time, "array1: ", result)
 
-print(superior)
-print(superior.at[indecies].set(inferior[indecies]))
-# Define the arrays
+array1 = np.array([1, 2, 3, 4, 5 ])
+result_np = np.array([1, 2, 3, 4, 5 ])
 
-# Subtract array2 from array1 element-wise
-# subtracted_array = jnp.subtract(superior, inferior)
-# subtracted_array = inferior[subtracted_array[:,0] == 0]
+for n in range(iterations):
+    start = time.time()
+    result_np = array1 + 10
+    end = time.time()
 
-# # Display the subtracted array and the combined array
-# print("Subtracted array:", subtracted_array ,subtracted_array.size)
+vec_time = end - start
+print("Vec time:     ", vec_time, "result: ", result_np)
+
+array1 = jnp.array([1, 2, 3, 4, 5 ])
+
+for n in range(iterations):
+    start = time.time()
+    result_jax = array1 + 10
+    end = time.time()
+
+jax_time = end - start
+print("Jax vec time:", jax_time, "result: ", result_jax)
