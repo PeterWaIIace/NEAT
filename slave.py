59e19a178d2900f6d1b01836552e668438dc35f9
Wallace <piotr.walas.prywatny@gmail.com>
2024-04-24 14:19:21 +0200

    removing old files

--- slave.py
+++
@@ -1,8 +0,0 @@
-# slave.py
-from mpi4py import MPI
-
-def main():
-    print("Spawned process running.")
-
-if __name__ == '__main__':
-    main()
--- src/utils.py
+++
@@ -1,6 +0,0 @@
-from enum import Enum
-
-class NodeTypes(Enum):
-    HIDDEN = 1 
-    INPUT = 2
-    OUTPUT = 3 
